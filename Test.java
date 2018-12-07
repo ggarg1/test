a7673342313997b6bf302c9813ad0a65991dd81e
Gaurav Garg <ggarg2@sapient.com>
2018-12-07 19:37:41 +0530

    New file

---
+++ Test.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+
+public class Test {
+
+	 private static int count = 0;
+
+	
+	public static void main(String[] args) {
+		
+				System.out.println("This is Gaurav Garg testing using GIT");
+				
+				System.out.println("Tetsing PR 3");
+																System.out.println("Tetsing PR 4");
+								System.out.println("Tetsing PR 3");
+				Process process =null;
+		
+			//System.out.println("This is Gaurav Garg testing using GIT");		
+				try {
+					process.getOutputStream().close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	}
+	
+	public void doSomething() {
+	    //...
+	    count++;  // Noncompliant
+	  }
+
+	  
+}
